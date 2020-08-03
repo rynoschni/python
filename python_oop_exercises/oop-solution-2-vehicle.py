02c50a54b491ffc7f84a7962bb5eb88c5a489941
Sean R Reid <sean@torchcodelab.com>
2020-08-03 15:38:24 -0400

    Updated vehicle exercise

--- python_oop_exercises/oop-solution-2-vehicle.py
+++ python_oop_exercises/oop-solution-2-vehicle.py
@@ -1,12 +1,16 @@
 # Exercise 2
 class Vehicle:
+    wheels = 4
+
     def __init__(self, make, model, year):
         self.make = make
         self.model = model
         self.year = year
 
-    def print_info(self):
-        print(f"{self.year} {self.make} {self.model}")
+    def __str__(self):
+        return "My %s %s is a %d model year" % (self.make, self.model, self.year)
 
-car = Vehicle("Volkswagen", "GTI", "2017")
-car.print_info()
+rex = Vehicle("Volkswagen", "GTI", 2005)
+print(rex)
+mickey = Vehicle("Volkswagen", "Golf (tuned)", 2018)
+print(mickey)
