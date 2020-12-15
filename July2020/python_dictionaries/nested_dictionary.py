a61bb85c719f2d3f21bbe61a5a0c07d5fdaeab0c
Sean R Reid <sean@torchcodelab.com>
2020-12-15 11:21:46 -0500

    Added pets folder and July2020 notes/solutions

---
+++ July2020/python_dictionaries/nested_dictionary.py
@@ -0,0 +1,16 @@
+digitalcrafts = {
+    'US': {
+        'Georgia': {
+            'Atlanta': '3423 Piedmont Road NE #420'
+        },
+        'Texas': {
+            'Houston': '3302 Canal St.'
+        }
+    }
+}
+
+digitalcrafts['US']['Florida'] = {
+    'Tampa': '123 Jake Stogdel House'
+}
+
+print(digitalcrafts['US'])
